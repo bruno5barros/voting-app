03669585a6aa407bfc5a450524a79db9b97a35a2
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2024-09-06 23:41:03 +0200

    Update start_drf.sh

--- scripts/start_drf.sh
+++ scripts/start_drf.sh
@@ -2,5 +2,7 @@
 set -e
 
 python manage.py migrate
+python manage.py setup_periodic_tasks
+python manage.py setup_voting_locker
 echo "Starting Django development server..."
 gunicorn -b :8000 --chdir /app lunch_voting.wsgi:application
