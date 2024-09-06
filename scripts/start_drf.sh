db760758115aa5981031eced47bb7f3db2b303a4
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2024-09-07 00:13:55 +0200

    Update start_drf.sh

--- scripts/start_drf.sh
+++ scripts/start_drf.sh
@@ -4,6 +4,6 @@ set -e
 python manage.py migrate
 python manage.py setup_periodic_tasks
 python manage.py setup_voting_locker
-python manage.py Create create_superuser
+python manage.py create_superuser
 echo "Starting Django development server..."
 gunicorn -b :8000 --chdir /app lunch_voting.wsgi:application
