b455586e180653b659b89c7da6f3648128faeb0e
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2024-09-10 19:57:35 +0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,17 +2,10 @@
 set -e
 
 python manage.py migrate
-
-if [ "$1" = "drf" ]; then
-    echo "Starting Django development server..."
-    gunicorn -b :80 --chdir /app lunch_voting.wsgi:application
-elif [ "$1" = "celery-worker" ]; then
-    echo "Starting Celery worker..."
-    exec celery -A lunch_voting worker -l info
-elif [ "$1" = "celery-beat" ]; then
-    echo "Starting Celery beat..."
-    exec celery -A lunch_voting beat --loglevel=info
-else
-    echo "Unknown command: $1"
-    exec "$@"  # Pass any unknown command to the shell
-fi
+python manage.py setup_periodic_tasks
+python manage.py setup_voting_locker
+python manage.py create_superuser
+echo "Starting Django development server..."
+gunicorn -b :8000 --chdir /app lunch_voting.wsgi:application
+celery -A lunch_voting worker -l info
+celery -A lunch_voting beat --loglevel=info
